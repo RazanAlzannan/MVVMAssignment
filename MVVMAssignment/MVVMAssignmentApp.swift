38c6408cdccc69e8ad7591a142f26ffef93ce94f
Razan <razanalzannan@gmail.com>
2023-05-24 14:27:59 +0300

    Initial Commit

---
+++ MVVMAssignment/MVVMAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAssignmentApp.swift
+//  MVVMAssignment
+//
+//  Created by Razan Mohammed Alzannan on 04/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
